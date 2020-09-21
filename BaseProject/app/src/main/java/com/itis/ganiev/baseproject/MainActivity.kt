3e86b1a414ab2d66ddbe69848cfb5084141fedb5
rishat19 <57725670+rishat19@users.noreply.github.com>
2020-09-21 09:38:52 +0300

    HW-02

--- BaseProject/app/src/main/java/com/itis/ganiev/baseproject/MainActivity.kt
+++ BaseProject/app/src/main/java/com/itis/ganiev/baseproject/MainActivity.kt
@@ -2,11 +2,24 @@ package com.itis.ganiev.baseproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editInfo.setOnClickListener {
+            if (houseInfo.visibility == View.VISIBLE) {
+                houseInfo.visibility = View.INVISIBLE
+                editHouseInfo.setText(houseInfo.text)
+                editHouseInfo.visibility = View.VISIBLE
+            } else {
+                editHouseInfo.visibility = View.INVISIBLE
+                houseInfo.text = editHouseInfo.text
+                houseInfo.visibility = View.VISIBLE
+            }
+        }
     }
 }
