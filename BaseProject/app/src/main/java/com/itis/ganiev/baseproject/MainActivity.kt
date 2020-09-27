72ea790a18d0418035d4d6719026ee390c7404ca
rishat19 <57725670+rishat19@users.noreply.github.com>
2020-09-27 14:32:05 +0300

    HW-02 (added SplashActivity, LoginActivity)

--- BaseProject/app/src/main/java/com/itis/ganiev/baseproject/MainActivity.kt
+++ BaseProject/app/src/main/java/com/itis/ganiev/baseproject/MainActivity.kt
@@ -10,6 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (intent != null) {
+            photo.setImageResource(intent.getIntExtra("photoId", R.drawable.user))
+            name.text = intent.getStringExtra("name")
+            if (intent.getBooleanExtra("flag", false)) {
+                schoolInfo.text = intent.getStringExtra("school")
+                houseInfo.text = intent.getStringExtra("house")
+                placeInfo.text = intent.getStringExtra("place")
+                subscriptionsInfo.text = intent.getStringExtra("subscriptions")
+            }
+        }
+
         editInfo.setOnClickListener {
             if (houseInfo.visibility == View.VISIBLE) {
                 houseInfo.visibility = View.INVISIBLE
@@ -21,5 +33,6 @@ class MainActivity : AppCompatActivity() {
                 houseInfo.visibility = View.VISIBLE
             }
         }
+
     }
 }
