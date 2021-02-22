78a6464de83698028bd56b64d92e9fe58e6dd5ef
rishat19 <57725670+rishat19@users.noreply.github.com>
2021-02-23 00:53:50 +0300

    HW-09 Weather Forecaster

--- BaseProject/app/src/main/java/com/itis/ganiev/baseproject/MainActivity.kt
+++ BaseProject/app/src/main/java/com/itis/ganiev/baseproject/MainActivity.kt
@@ -2,11 +2,26 @@ package com.itis.ganiev.baseproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setFragment(SearchFragment.newInstance())
     }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
 }
